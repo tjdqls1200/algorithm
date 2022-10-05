6e117f8c9b40cdda5b52b2f1fda3d0edabc1183a
code-tree <dltjdqls1200@naver.com>
2022-10-05 13:26:13 +0900

    백준 시나리오 문제풀이

---
+++ src/com/baekjoon/simulation/P_2884.java
@@ -0,0 +1,25 @@
+package com.baekjoon.simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class P_2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int h = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        m -= 45;
+        if (m < 0) {
+            h -= 1;
+            m += 60;
+        }
+        if (h == -1) {
+            h = 23;
+        }
+        System.out.println(h + " " + m);
+    }
+}
