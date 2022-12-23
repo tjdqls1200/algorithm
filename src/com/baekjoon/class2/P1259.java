af106b0cdc9cfcc9d6c12bc5d0ca52103fb25a1e
code-tree <dltjdqls1200@naver.com>
2022-12-23 14:01:44 +0900

    백준 class2 문제풀이

---
+++ src/com/baekjoon/class2/P1259.java
@@ -0,0 +1,36 @@
+package com.baekjoon.class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1259 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String n = br.readLine();
+        while (!"0".equals(n)) {
+            sb.append(solution(n)).append("\n");
+            n = br.readLine();
+        }
+
+        System.out.println(sb);
+    }
+
+    private static String solution(String n) {
+        char[] chars = n.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start <= end) {
+            if (chars[start] != chars[end]) {
+                return "no";
+            }
+            start++;
+            end--;
+        }
+
+        return "yes";
+    }
+}
